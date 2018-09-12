75be2d31fac0780db00a624e908a32a4d1834708
initialing <zqt_scholes@163.com>
2018-09-12 19:06:58 +0800

    completed the first slide

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="home" @click='test'>
+    <div class="home">
+        <div class='logo'>
+            <a class='logo-link' href='https://github.com/initialing' target="blank"></a>
+        </div>
         <swiper :options="swiperOption" ref="mySwiper">
-            <swiper-slide id='slide1'>
+            <swiper-slide id='slide-1'>
+                <div class="slide-1-body">
+                    <h5>关于我</h5>
+                    <hr/>
+                    <p>郑秋桐</p>
+                    <p>一名想成为前端工程师的同学</p>
+                    <p>zqt_scholes@163.com</p>
+                </div>
             </swiper-slide>
             <swiper-slide>I'm Slide 2</swiper-slide>
             <swiper-slide>I'm Slide 3</swiper-slide>
